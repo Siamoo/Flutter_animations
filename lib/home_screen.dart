416a46c1b591b6b8b88845bf3751639a632a3d59
Siamoo <walidsyam.f@gmail.com>
2025-09-07 07:22:21 +0300

    TweenAnimationBuilder -> Tween<double> (child)

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animations/counter_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,15 +8,30 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  double _dimension = 100;
+  double _begin = 100;
+  double _end = 200;
+
+    late final Widget _child; // 👈 keep a reference
+  @override
+  @override
+  void initState() {
+    super.initState();
+    _child = _getColumnWidget();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Animated Foo Basics")),
+      appBar: AppBar(title: const Text("Tween Animations")),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [_buttonWidget()],
+          children: [
+            SizedBox(height: 30),
+            _containerWidget(),
+            Spacer(),
+            _buttonWidget(),
+            SizedBox(height: 30),
+          ],
         ),
       ),
     );
@@ -26,9 +40,47 @@ class _HomeScreenState extends State<HomeScreen> {
   ElevatedButton _buttonWidget() {
     return ElevatedButton(
       onPressed: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => CounterScreen()));
+        setState(() {
+          _begin = (_begin == 100) ? 200 : 100;
+          _end = (_end == 100) ? 200 : 100;
+        });
       },
       child: const Text("Animate"),
     );
   }
+
+Widget _containerWidget() {
+  return TweenAnimationBuilder(
+    curve: Curves.bounceInOut,
+    tween: Tween<double>(begin: _begin, end: _end),
+    duration: Duration(seconds: 2),
+    child: _child,   
+    builder: (context, value, child) {
+      return Container(
+        height: value,
+        width: value,
+        decoration: _getBoxDecoration(),
+        child: child,   // we use the child from the builder to not rebuild it every time
+      );
+    },
+  );
+}
+
+}
+
+Widget _getColumnWidget() {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: const [
+      Text("Flutter", style: TextStyle(color: Colors.white, fontSize: 20)),
+      Text("Animation", style: TextStyle(color: Colors.white, fontSize: 20)),
+    ],
+  );
+}
+
+_getBoxDecoration() {
+  return BoxDecoration(
+    color: Colors.blueAccent,
+    borderRadius: BorderRadius.circular(10),
+  );
 }
