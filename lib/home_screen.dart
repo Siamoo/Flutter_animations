c29c00da80be081401b9e2df391191168bf2c827
Siamoo <walidsyam.f@gmail.com>
2025-09-09 03:42:34 +0300

    Foo Transition -> AlignTransition ( Dispose Controller & Remove Listener )

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,36 +8,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
-  late Animation<AlignmentGeometry> _greenAnimation;
   late Animation<AlignmentGeometry> _redAnimation;
-  late AnimationController _greenController;
   late AnimationController _redController;
+
+  @override
+  void dispose() {
+    _redController.dispose();
+    super.dispose();
+  }
 
   @override
   void initState() {
     super.initState();
 
-    _greenController = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 1),
-      reverseDuration: Duration(seconds: 1),
-    );
     _redController = AnimationController(
       vsync: this,
       duration: Duration(seconds: 2),
       reverseDuration: Duration(seconds: 2),
     );
-    _greenAnimation =
-        Tween<AlignmentGeometry>(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-        ).animate(
-          CurvedAnimation(
-            parent: _greenController,
-            curve: Curves.easeInOut,
-            reverseCurve: Curves.easeInOut,
-          ),
-        );
+
     _redAnimation =
         Tween<AlignmentGeometry>(
           begin: Alignment.centerLeft,
@@ -49,60 +38,93 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
             // reverseCurve: Curves.easeOutBack,
           ),
         );
-        
-    _greenController.addListener(() {
-      if (_greenController.value >= 0.5 && _redController.value== 0 ) {
-        _redController.forward();
-      }
-      if (_greenController.value <= 0.5 && _redController.value == 1) {
-        _redController.reverse();
-      }
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Foo Transition")),
-      body: Column(children: [_stackWidget(), _controlButton()]),
-    );
-  }
-
-  Widget _stackWidget() {
-    return Expanded(
-      child: Stack(
-        alignment: Alignment.bottomCenter,
+      body: Column(
         children: [
-          AlignTransition(
-            alignment: _greenAnimation,
-            child: CircleAvatar(backgroundColor: Colors.green),
+          _stackWidget(),
+
+          ElevatedButton(
+            onPressed: () {
+              if (_redController.isCompleted) {
+                _redController.reverse();
+              } else {
+                _redController.forward();
+              }
+            },
+
+            child: Text("Forward"),
           ),
-          AlignTransition(
-            alignment: _redAnimation,
-            child: CircleAvatar(backgroundColor: Colors.red),
-          ),
+          _statusListenerControlButton(),
+          _valueListenerControlButton(),
         ],
       ),
     );
   }
 
-  Widget _controlButton() {
-    return Wrap(
-      alignment: WrapAlignment.spaceBetween,
+  Widget _stackWidget() {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        AlignTransition(
+          alignment: _redAnimation,
+          child: CircleAvatar(backgroundColor: Colors.red),
+        ),
+      ],
+    );
+  }
+
+  void _statusListener(AnimationStatus animationStatus) {
+    print('==================$animationStatus');
+  }
+
+  _statusListenerControlButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
         ElevatedButton(
           onPressed: () {
-            _greenController.forward();
+            _redController.addStatusListener(_statusListener);
+            // _redAnimation.addStatusListener(_statusListener); can do it for AlignmentGeometry although
           },
-
-          child: Text("Forward"),
+          child: Text("Add Status Listener"),
         ),
-        SizedBox(width: 30),
         ElevatedButton(
           onPressed: () {
-            _greenController.reverse();
+            _redController.removeStatusListener(_statusListener);
+            // _redAnimation.removeStatusListener(_statusListener); can do it for AlignmentGeometry although
           },
-          child: Text("Reverse"),
+          child: Text("Remove Status Listener"),
+        ),
+      ],
+    );
+  }
+
+  void _valueListener() {
+    print('==================${_redController.value}');
+  }
+
+  _valueListenerControlButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            _redController.addListener(_valueListener);
+            // _redAnimation.addListener(_valueListener); can do it for AlignmentGeometry although
+          },
+          child: Text("Add Value Listener"),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            _redController.removeListener(_valueListener);
+            // _redAnimation.removeListener(_valueListener); can do it for AlignmentGeometry although
+          },
+          child: Text("Remove Value Listener"),
         ),
       ],
     );
